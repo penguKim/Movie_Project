2e95c3d359d311f10d0a60950abef5129c6b3902
박가영 <gypark619@gmail.com>
2023-12-12 12:58:05 +0900

    박가영 - 회원가입, 공지사항 Controller, Service, Mapper, Vo 수정

---
+++ Movie_Project/src/main/java/com/itwillbs/c5d2308t1/mapper/JoinMapper.java
@@ -0,0 +1,13 @@
+package com.itwillbs.c5d2308t1.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.itwillbs.team_project.vo.MemberVO;
+
+@Mapper
+public interface JoinMapper {
+
+	// 회원정보 등록 - 추상메서드 정의
+	int insertMember(MemberVO member);
+
+}
---
+++ Movie_Project/src/main/java/com/itwillbs/c5d2308t1/service/JoinService.java
@@ -0,0 +1,25 @@
+package com.itwillbs.c5d2308t1.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.team_project.mapper.JoinMapper;
+import com.itwillbs.team_project.vo.MemberVO;
+
+@Service
+public class JoinService {
+	// 마이바티스를 통해 SQL 구문 처리를 담당할 XXXMapper.xml 파일과 연동되는
+	// XXXMapper 객체(인터페이스)를 자동 주입받기 위해 @Autowired 어노테이션으로 멤버변수 선언
+	@Autowired
+	private JoinMapper mapper;
+
+	public int registMember(MemberVO member) {
+		System.out.println("JoinService - registMember()");
+		
+		// JoinMapper - insertMember() 메서드를 호출하여 회원정보 등록 요청
+		// => 파라미터 : MemberVO 객체   리턴타입 : int
+		return mapper.insertMember(member);
+	}
+	
+	
+}
