4ea23d732cc22521c1b771f81ee773b716066b8a
gypark619 <gypark619@gmail.com>
2023-12-22 00:25:05 +0900

    박가영 - 자주묻는질문관리 수정기능 추가, 등록 오류 미해결

--- Movie_Project/src/main/java/com/itwillbs/c5d2308t1/controller/AdminController.java
+++ Movie_Project/src/main/java/com/itwillbs/c5d2308t1/controller/AdminController.java
@@ -1,13 +1,22 @@
 package com.itwillbs.c5d2308t1.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -16,13 +25,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
 import com.itwillbs.c5d2308t1.service.AdminService;
 import com.itwillbs.c5d2308t1.vo.CsVO;
 import com.itwillbs.c5d2308t1.vo.MemberVO;
 import com.itwillbs.c5d2308t1.vo.MoviesVO;
 import com.itwillbs.c5d2308t1.vo.PageCount;
 import com.itwillbs.c5d2308t1.vo.PageDTO;
+import com.itwillbs.c5d2308t1.vo.PageInfo;
 import com.itwillbs.c5d2308t1.vo.TheaterVO;
 
 @Controller
@@ -330,31 +342,211 @@ public class AdminController {
 	// ****************** 자주 묻는 질문 관리 페이지 *************
 	// 관리자페이지 자주묻는질문 관리 페이지로 이동
 	@GetMapping("adminFaq")
-	public String adminFaq() {
+	public String adminFaq(CsVO cs, Model model, HttpServletRequest request,
+			@RequestParam(defaultValue = "1") int pageNum, HttpSession session) {
+		// 세션아이디 판별하여 관리자(admin)가 아니면 접근을 막기
+		String sId = (String)session.getAttribute("sId");
+		if(sId == null || !sId.equals("admin")) {
+			model.addAttribute("msg", "잘못된 접근입니다!");
+			return "fail_back";
+		}
+		
+		// 자주묻는질문 버튼을 눌렀을 때 cs_type을 자주묻는질문으로 설정
+		cs.setCs_type("자주묻는질문");
+		
+		// 한 페이지에서 표시할 글 목록 갯수 지정 (테스트)
+		int listLimit = 5;
+		
+		// 조회 시작 행번호
+		int startRow = (pageNum - 1) * listLimit;
+		
+		// CsService - getFaqList() 메서드 호출하여 자주 묻는 질문 출력
+		// => 파라미터 : 시작행번호, 목록갯수   리턴타입 : List<CsVO>(noticeList)
+		List<HashMap<String, Object>> faqList = service.getCsList(cs, startRow, listLimit);
+	//	System.out.println(noticeList);
+		
+		// ======================================================
+		int listCount = service.getCsTypeListCount(cs);
+		int pageListLimit = 5;
+		int maxPage = listCount / listLimit + ((listCount % listLimit) > 0 ? 1 : 0);
+		int startPage = (pageNum - 1) / pageListLimit * pageListLimit + 1;
+		int endPage = startPage + pageListLimit - 1;
+		if(endPage > maxPage) {
+			endPage = maxPage;
+		}
+		
+		// 계산된 페이징 처리 관련 값을 PageInfo 객체에 저장
+		PageInfo pageInfo = new PageInfo(listCount, maxPage, pageListLimit, startPage, endPage);
+		// ------------------------------------------------------
+		// 글목록(List 객체)과 페이징정보(pageInfo 객체) 를 request 객체에 저장
+		model.addAttribute("pageInfo", pageInfo);
+		model.addAttribute("faqList", faqList);
+		model.addAttribute("pageNum", pageNum);
+		
 		return "admin/admin_board_faq";
 	}
 
-	// 관리자페이지 자주묻는질문 글 등록 페이지로 이동
-	@GetMapping("adminFaqWrt")
-	public String adminFaqWrt() {
+	// 관리자페이지 자주묻는질문 글쓰기 페이지로 이동
+	@GetMapping("adminFaqWriteForm")
+	public String adminFaqWriteForm(HttpSession session, Model model) {
+		// 세션아이디 판별하여 관리자(admin)가 아니면 접근을 막기
+		String sId = (String)session.getAttribute("sId");
+		if(sId == null || !sId.equals("admin")) {
+			model.addAttribute("msg", "잘못된 접근입니다!");
+			return "fail_back";
+		}
+		
 		return "admin/admin_board_faq_write";
 	}
 
-	@PostMapping("boardFaqRgst") // 자주묻는 질문 글쓰기 : admin_board_faq_write.jsp
-	public String boardFaqRgst() {
-		return "";
+	// 관리자페이지 자주묻는질문 글 등록하기
+	@PostMapping("boardFaqWritePro") // 자주묻는 질문 글쓰기 : admin_board_faq_write.jsp
+	public String boardFaqWritePro(CsVO cs, HttpSession session, Model model, 
+			@RequestParam(defaultValue = "0") int cs_id) {
+		// 세션아이디 판별하여 관리자(admin)가 아니면 접근을 막기
+		String sId = (String)session.getAttribute("sId");
+		if(sId == null || !sId.equals("admin")) {
+			model.addAttribute("msg", "잘못된 접근입니다!");
+			return "fail_back";
+		}
+		
+		// --------------------------------------------------------
+		// 파일업로드를 위한 준비
+		// resources 디렉토리 내에 upload 파일 생성
+		String uploadDir = "/resources/upload"; // 가상 디렉토리
+		String saveDir = session.getServletContext().getRealPath(uploadDir); // 실제 디렉토리
+		String subDir = "";
+//				
+		// 날짜별로 서브디렉토리 생성하기
+		LocalDate now = LocalDate.now();
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		subDir = now.format(dtf);
+		
+		saveDir += File.separator + subDir;
+		
+		// 해당 디렉토리가 존재하지 않을 때에만 자동생성
+		try {
+			Path path = Paths.get(saveDir); // 업로드 경로
+			Files.createDirectories(path); // Path 객체
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+//		// MultipartFile 타입 객체 꺼내기
+		MultipartFile mFile = cs.getMFile();
+		
+		// 파일명 중복을 방지하기 위해 난수 생성하기
+		cs.setCs_file("");
+		String fileName = UUID.randomUUID().toString() + "_" + mFile.getOriginalFilename();
+		
+		if(!mFile.getOriginalFilename().equals("")) {
+			cs.setCs_file(subDir + "/" + fileName);
+		}
+		
+		System.out.println("업로드 파일명 확인 : " + cs.getCs_file());
+		
+		
+		if(cs_id != 0) {
+			// AdminService - registBoard() 메서드 호출하여 문의글 수정 요청
+			cs.setCs_id(cs_id);
+			int updateCount = service.updateBoard(cs);
+			System.out.println("updateCount :" + updateCount);
+			
+			// 등록 실패 시 fail_back.jsp 페이지로 포워딩(디스패치)
+			// => 포워딩 출력할 오류메세지를 "msg" 라는 속성명으로 Model 객체에 저장
+			//    (현재 메서드 파라미터에 Model 타입 파라미터 변수 선언 필요)
+			if(updateCount > 0) {
+				// 파일이 있을 경우에만 파일 생성
+				try {
+					if(!mFile.getOriginalFilename().equals("")) {
+						mFile.transferTo(new File(saveDir, fileName));
+					}
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				return "redirect:/adminFaq";
+				
+			} else {			
+				model.addAttribute("msg", "문의글 수정 실패!");
+				return "fail_back";
+			}
+			
+		} else {
+			// AdminService - registBoard() 메서드 호출하여 문의글 등록 요청
+			int insertCount = service.registBoard(cs);
+			System.out.println("insertCount :" +insertCount);
+			
+			// 등록 실패 시 fail_back.jsp 페이지로 포워딩(디스패치)
+			// => 포워딩 출력할 오류메세지를 "msg" 라는 속성명으로 Model 객체에 저장
+			//    (현재 메서드 파라미터에 Model 타입 파라미터 변수 선언 필요)
+			if(insertCount > 0) {
+				// 파일이 있을 경우에만 파일 생성
+				try {
+					if(!mFile.getOriginalFilename().equals("")) {
+						mFile.transferTo(new File(saveDir, fileName));
+					}
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+				return "redirect:/adminFaq";
+				
+			} else {			
+				model.addAttribute("msg", "문의글 등록 실패!");
+				return "fail_back";
+			}
+			
+		}
+		
 	}
 
-	// 관리자페이지 자주묻는질문 상세 조회 및 수정 페이지로 이동
-	@GetMapping("adminFaqMod")
-	public String adminFaqMod() {
-		return "admin/admin_board_faq_modify";
+	
+	// 관리자페이지 자주묻는질문 상세 조회 페이지로 이동
+	@GetMapping("adminFaqView")
+	public String adminFaqView(CsVO cs, HttpSession session, Model model) {
+		// 세션아이디 판별하여 관리자(admin)가 아니면 접근을 막기
+		String sId = (String)session.getAttribute("sId");
+		if(sId == null || !sId.equals("admin")) {
+			model.addAttribute("msg", "잘못된 접근입니다!");
+			return "fail_back";
+		}
+
+		HashMap<String, Object> csFaqDetail = service.csFaqDetailPage(cs);
+		System.out.println(csFaqDetail);
+		model.addAttribute("csFaqDetail", csFaqDetail);
+		
+		return "admin/admin_board_faq_view";
 	}
 	
-	@PostMapping("boardFaqModOrDlt") // 자주 묻는 질문 상세조회/수정 : admin_board_faq_modify.jsp
-	public String boardFaqModOrDlt() {
-		return "";
+	@PostMapping("boardFaqModifyForm") // 자주 묻는 질문 수정 : admin_board_faq_write.jsp 재사용
+	public String boardFaqModifyForm(CsVO cs, HttpSession session, Model model, 
+			String cs_type, int cs_type_list_num) {
+		// 세션아이디 판별하여 관리자(admin)가 아니면 접근을 막기
+		String sId = (String)session.getAttribute("sId");
+		if(sId == null || !sId.equals("admin")) {
+			model.addAttribute("msg", "잘못된 접근입니다!");
+			return "fail_back";
+		}
+		
+		// 파라미터로 가져온 게시글 종류와 게시글 번호 csVO에 저장하기
+		cs.setCs_type(cs_type);
+		cs.setCs_type_list_num(cs_type_list_num);
+		
+		// DB에 등록되어있는 값 가져오기
+		HashMap<String, Object> csFaqDetail = service.csFaqDetailPage(cs);
+		System.out.println(csFaqDetail);
+		model.addAttribute("csFaqDetail", csFaqDetail);
+		
+		
+		return "admin/admin_board_faq_write";
 	}
+	
+	
 
 	
 	// ===========================================================================================
@@ -366,25 +558,25 @@ public class AdminController {
 	}
 
 	// 관리자페이지 공지사항 글등록 페이지로 이동
-	@GetMapping("adminNoticeWrt")
-	public String adminNoticeWrt() {
+	@GetMapping("adminNoticeWriteForm")
+	public String adminNoticeWriteForm() {
 		return "admin/admin_board_notice_write";
 	}
 
-	@PostMapping("boardNoticeRgst") // 공지사항 글쓰기 : admin_board_notice_write.jsp
-	public String boardNoticeRgst() {
+	@PostMapping("adminNoticeWritePro") // 공지사항 글쓰기 : admin_board_notice_write.jsp
+	public String adminNoticeWritePro() {
 		return "";
 	}
 
-	// 관리자페이지 공지사항 상세 조회 및 수정 페이지로 이동
+	// 관리자페이지 공지사항 상세 조회 페이지로 이동
 	@GetMapping("adminNoticeDtl")
 	public String adminNoticeDtl() {
 		return "admin/admin_board_notice_detail";
 	}
 	
-	@PostMapping("boardNoticeModOrDlt") // 공지사항 상세조회/수정 : admin_board_notice_detail.jsp
-	public String boardNoticeModOrDlt() {
-		return "";
+	@PostMapping("boardNoticeModify") // 공지사항 수정 : admin_board_notice_write.jsp 재사용
+	public String boardNoticeModify() {
+		return "admin/admin_board_notice_write";
 	}
 	
 	// ===========================================================================================
--- Movie_Project/src/main/webapp/WEB-INF/views/admin/admin_board_faq.jsp
+++ Movie_Project/src/main/webapp/WEB-INF/views/admin/admin_board_faq.jsp
@@ -1,6 +1,8 @@
 <%-- admin_board_faq.jsp --%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +11,50 @@
 <%-- 외부 CSS 파일 연결하기 --%>
 <link href="${pageContext.request.contextPath}/resources/css/default.css" rel="stylesheet" type="text/css">
 <link href="${pageContext.request.contextPath}/resources/css/admin.css" rel="stylesheet" type="text/css">
+<script src="${pageContext.request.contextPath}/resources/js/jquery-3.7.1.js"></script>
+<script>
+	$(function() {
+		<%-- faq 세부항목별 보기 버튼을 눌렀을 때 세부항목별로 모아서 보여주는 기능 --%> 
+		$(".faqButton").click(function() {
+			<%-- 선택한 버튼의 이름값 가져와서 변수에 저장 --%>
+			let buttonName = $(this).attr("name");
+			
+			$.ajax({
+				url: "faqDetail", <%-- csController 에 있는 매핑 주소 사용 --%>
+				data: { buttonName: buttonName },
+				success: function(data) {
+					<%-- 세부항목을 눌렀을 때 --%>
+					$("table>tbody").empty();
+					
+					for(let faq of data) {
+						let date = new Date(faq.cs_date);
+						let year = date.getFullYear(); // 연도
+						let month = date.getMonth() + 1; // 월 (0부터 시작하므로 1을 더해줌)
+						let day = date.getDate(); // 일
+	
+						// 가공된 날짜 형식으로 출력
+						let formattedDate = year + "-" + month + "-" + day;
+						
+						$("table>tbody").append (
+							"<tr>"
+							+	"<td>" + faq.cs_type_list_num + "</td>"
+							+	"<td>" + faq.cs_type_detail + "</td>"
+							+	"<td class='post_name'><a href='adminFaqView?cs_type=" + faq.cs_type + "&cs_type_list_num=" + faq.cs_type_list_num + "&pageNum=${pageNum}'>" + faq.cs_subject + "</a></td>"
+							+	"<td>" + faq.member_id + "</td>"
+							+	"<td>" + formattedDate + "</td>"
+							+"</tr>"
+						);
+					}		
+							
+				},
+				error: function(xhr, status, error) {
+			      // 요청이 실패한 경우 처리할 로직
+			      console.log("AJAX 요청 실패:", error); // 예시: 에러 메시지 출력
+				}
+			});
+		});
+	});
+</script>			
 </head>
 <body>
 	<div id="wrapper">
@@ -28,110 +74,85 @@
 			<div id="admin_main">
 				<nav id="faq_button">
 					<ul>
-						<li><a href="#div01"><input type="button" value="전체"></a></li> <%-- 전체 질문 보기 --%>
-						<li><a href="#div02"><input type="button" value="예매"></a></li> <%-- 예매 관련 질문 모아보기 --%>
-						<li><a href="#div03"><input type="button" value="관람권"></a></li> <%-- 관람권 관련 질문 모아보기 --%>
-						<li><a href="#div03"><input type="button" value="할인혜택"></a></li> <%-- 할인 관련 질문 모아보기 --%>
-						<li><a href="#div03"><input type="button" value="영화관이용"></a></li> <%-- 영화관 관련 질문 모아보기 --%>
+						<li><input type="button" value="전체" class="faqButton" name="전체"></li> <%-- 전체 질문 보기 --%>
+						<li><input type="button" value="예매" class="faqButton" name="예매"></li> <%-- 예매 관련 질문 모아보기 --%>
+						<li><input type="button" value="관람권" class="faqButton" name="관람권"></li> <%-- 관람권 관련 질문 모아보기 --%>
+						<li><input type="button" value="할인혜택" class="faqButton" name="할인혜택"></li> <%-- 할인 관련 질문 모아보기 --%>
+						<li><input type="button" value="영화관이용" class="faqButton" name="영화관이용"></li> <%-- 영화관 관련 질문 모아보기 --%>
 					</ul>
 				</nav>
 				<table border="1">
-					<tr>
-						<th width="30">번호</th>
-						<th width="100">유형</th>
-						<th width="370">제목</th>
-						<th width="100">작성자</th>
-						<th width="120">등록일</th>
-					</tr>
-					<tr>
-						<td>100</td>
-						<td>영화관이용</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>admin</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
-					<tr>
-						<td>1</td>
-						<td>관람권</td>
-						<td class="post_name"><a href="adminFaqMod">클릭시 자주묻는질문 상세 조회/수정페이지로 이동</a></td>
-						<td>관리자</td>
-						<td>2023-11-16</td>
-					</tr>
+					<thead>
+						<tr>
+							<th width="30">번호</th>
+							<th width="100">유형</th>
+							<th width="370">제목</th>
+							<th width="100">작성자</th>
+							<th width="120">등록일</th>
+						</tr>
+					</thead>					
+					<tbody>	
+						<c:choose>
+							<c:when test="${empty faqList}">
+								자주 묻는 질문 없음						
+							</c:when>
+							<c:otherwise>
+								<%-- FQA 질문 리스트 List<CsVO> 객체(faqList) 활용하여 목록 출력 --%>
+								<c:forEach var="faq" items="${faqList}">
+									<tr>
+										<td>${faq.cs_type_list_num}</td>
+										<td>${faq.cs_type_detail}</td>
+										<td class="post_name"><a href="adminFaqView?cs_type=${faq.cs_type}&cs_type_list_num=${faq.cs_type_list_num}&pageNum=${pageNum}">${faq.cs_subject}</a></td>
+										<td>${faq.member_id}</td>
+										<td>
+											<fmt:parseDate value='${faq.cs_date}' pattern="yyyy-MM-dd" var='cs_date'/>
+											<fmt:formatDate value="${cs_date}" pattern="yyyy-MM-dd"/>
+										</td>
+									</tr>
+								</c:forEach>
+							</c:otherwise>
+						</c:choose>
+					</tbody>
 				</table>
 				<div id="admin_writer">
-					<a href="adminFaqWrt"><input type="button" value="글쓰기"></a>
+					<a href="adminFaqWriteForm"><input type="button" value="글쓰기"></a>
 				</div>
 				<div class="pagination">
-					<a href="#">&laquo;</a>
-					<a href="#">1</a>
-					<a class="active" href="#">2</a>
-					<a href="#">3</a>
-					<a href="#">4</a>
-					<a href="#">5</a>
-					<a href="#">&raquo;</a>
+					<%-- '<<' 버튼 클릭 시 현체 페이지보다 한 페이지 앞선 페이지 요청 --%>
+					<%-- 다만, 페이지 번호가 1일 경우 비활성화 --%>		
+					<c:choose>
+						<c:when test="${pageNum eq 1}">
+							<a href="" >&laquo;</a>					
+						</c:when>
+						<c:otherwise>
+							<a href="adminFaq?pageNum=${pageNum-1}" >&laquo;</a>
+						</c:otherwise>				
+					</c:choose>
+					<%-- 현재 페이지가 저장된 pageInfo 객체를 통해 페이지 번호 출력 --%>
+					<c:forEach var="i" begin="${pageInfo.startPage}" end="${pageInfo.endPage}">
+						<%-- 각 페이지마다 하이퍼링크 설정(페이지번호를 pageNum 파라미터로 전달) --%>
+						<%-- 단, 현재 페이지는 하이퍼링크 제거하고 굵게 표시 --%>
+						<c:choose>
+							<%-- 현재 페이지번호와 표시될 페이지번호가 같을 경우 판별 --%>
+							<c:when test="${pageNum eq i}">
+								<a class="active" href="">${i}</a> <%-- 현재 페이지 번호 --%>
+							</c:when>
+							<c:otherwise>
+								<a href="adminFaq?pageNum=${i}">${i}</a> <%-- 다른 페이지 번호 --%>
+							</c:otherwise>
+						</c:choose>
+					</c:forEach>
+					
+					<%-- '>>' 버튼 클릭 시 현체 페이지보다 한 페이지 다음 페이지 요청 --%>
+					<%-- 다만, 페이지 번호가 마지막 경우 비활성화 --%>		
+					<c:choose>
+						<c:when test="${pageNum eq pageInfo.maxPage}">
+							<a href="" >&raquo;</a>					
+						</c:when>
+						<c:otherwise>
+							<a href="adminFaq?pageNum=${pageNum+1}" >&raquo;</a>
+						</c:otherwise>				
+					</c:choose>
 				</div>
 			</div>
 			<footer>
